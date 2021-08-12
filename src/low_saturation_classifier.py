7ae46ae94d9d8a189f7a3b9b3162217541cbeca8
Barış Çelik <88535469+abarcel@users.noreply.github.com>
2021-08-12 13:41:22 +0300

    Update low_saturation_classifier.py

--- src/low_saturation_classifier.py
+++ src/low_saturation_classifier.py
@@ -9,17 +9,18 @@ class LowSaturationClassifier:
     extractor = ColorExtractor()
     arr = []
 
-    def is_gray(self, file):
-        col = self.extractor.get_color(file, 3)
+    def is_gray(self, file, k=3):
+        col, pr = self.extractor.get_color(file, 3) 
         arr = [i[1] for i in col]
-
+        
         gfactor = 0
         for i in arr:
             gfactor += (abs(i[0] - i[1]))
             gfactor += (abs(i[1] - i[2]))
             gfactor += (abs(i[0] - i[2]))
         print('grayfactor is ' + str(gfactor))
-        if gfactor < 125:
+        print('pearson cor. is ' + str(pr))
+        if gfactor < 150 and np.min(pr) > 0.95:
             return True
         else:
             return False
